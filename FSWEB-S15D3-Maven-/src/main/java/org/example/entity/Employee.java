92be2244f6b2c29301da1a1933824d490b8ca4cf
EmrUZN <165069755+EmrUZN@users.noreply.github.com>
2024-11-01 16:41:28 +0300

    Add files via upload

---
+++ FSWEB-S15D3-Maven-/src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,49 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Employee employee = (Employee) object;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
